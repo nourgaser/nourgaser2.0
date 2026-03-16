b2bc29d540c96bd7c8ece708769a17cb44695e50
nourgaser <work@nourgaser.com>
2026-03-16 10:05:35 +0200

    Add sitemap integration and enhance SEO metadata across pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import svelte from '@astrojs/svelte';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), mdx()],
+  integrations: [svelte(), mdx(), sitemap()],
   image: {
     responsiveStyles: true,
   },
